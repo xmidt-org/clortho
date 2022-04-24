f00f507b233da36ccb0b6efdc3bb498a45186b78
johnabass <johnbass1017@yahoo.com>
2022-04-23 17:41:26 -0700

    key ring mock

--- mocks_test.go
+++ mocks_test.go
@@ -34,3 +34,66 @@ func (m *mockParser) Parse(format string, data []byte) ([]Key, error) {
 func (m *mockParser) ExpectParse(format string, data []byte) *mock.Call {
 	return m.On("Parse", format, data)
 }
+
+type mockFetcher struct {
+	mock.Mock
+}
+
+func (m *mockFetcher) Fetch(ctx context.Context, location string, prev ContentMeta) ([]Key, ContentMeta, error) {
+	args := m.Called(ctx, location, prev)
+	return args.Get(0).([]Key),
+		args.Get(1).(ContentMeta),
+		args.Error(2)
+}
+
+func (m *mockFetcher) ExpectFetch(ctx context.Context, location string, prev ContentMeta) *mock.Call {
+	return m.On("Fetch", ctx, location, prev)
+}
+
+type mockKeyRing struct {
+	mock.Mock
+}
+
+func (m *mockKeyRing) Get(kid string) (Key, bool) {
+	args := m.Called(kid)
+	return args.Get(0).(Key), args.Bool(1)
+}
+
+func (m *mockKeyRing) Len() int {
+	return m.Called().Int(0)
+}
+
+func (m *mockKeyRing) OnRefreshEvent(event RefreshEvent) {
+	m.Called(event)
+}
+
+func (m *mockKeyRing) ExpectOnRefreshEvent(event RefreshEvent) *mock.Call {
+	return m.On("OnRefreshEvent", event)
+}
+
+func (m *mockKeyRing) OnResolveEvent(event ResolveEvent) {
+	m.Called(event)
+}
+
+func (m *mockKeyRing) ExpectOnResolveEvent(event ResolveEvent) *mock.Call {
+	return m.On("OnResolveEvent", event)
+}
+
+func (m *mockKeyRing) Add(keys ...Key) int {
+	actual := make([]interface{}, 0, len(keys))
+	for _, k := range keys {
+		actual = append(actual, k)
+	}
+
+	args := m.Called(actual...)
+	return args.Int(0)
+}
+
+func (m *mockKeyRing) ExpectAdd(keys ...Key) *mock.Call {
+	expect := make([]interface{}, 0, len(keys))
+	for _, k := range keys {
+		expect = append(expect, k)
+	}
+
+	return m.On("Add", expect...)
+}
